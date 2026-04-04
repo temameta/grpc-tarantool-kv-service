aeb3a42b0bd34f859fb598fdd21c2d261213624d
tema <sezemov2@gmail.com>
2026-04-05 00:07:06 +0300

    feat: implement service from gRPC proto

---
+++ src/main/java/org/example/kv/KvServiceImpl.java
@@ -0,0 +1,98 @@
+package org.example.kv;
+
+import com.google.protobuf.ByteString;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import io.tarantool.client.box.TarantoolBoxClient;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class KvServiceImpl extends KvServiceGrpc.KvServiceImplBase {
+    private final TarantoolBoxClient tarantoolBoxClient;
+
+    @Override
+    public void put(PutRequest request, StreamObserver<PutResponse> responseObserver) {
+        try {
+            String key = request.getKey();
+            byte[] value = request.hasValue() ? request.getValue().toByteArray() : null;
+            List<?> tuple = Arrays.asList(key, value);
+            var puttedTuple = tarantoolBoxClient.space("KV").replace(tuple).join();
+            boolean isPutted = puttedTuple.get() != null && !puttedTuple.get().isEmpty();
+            PutResponse success = PutResponse.newBuilder().setSuccess(isPutted).build();
+            responseObserver.onNext(success);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void get(GetRequest request, StreamObserver<GetResponse> responseObserver) {
+        try {
+            String key = request.getKey();
+            var tarantoolResponse = tarantoolBoxClient.space("KV").select(List.of(key)).join();
+            if (tarantoolResponse.get().isEmpty()) responseObserver.onError(Status.NOT_FOUND.asException());
+            else {
+                var value = tarantoolResponse.get().getFirst().get().get(1);
+                GetResponse.Builder response = GetResponse.newBuilder();
+                if (value != null) response.setValue(ByteString.copyFrom((byte[]) value));
+                responseObserver.onNext(response.build());
+                responseObserver.onCompleted();
+            }
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void delete(DeleteRequest request, StreamObserver<DeleteResponse> responseObserver) {
+        try {
+            String key = request.getKey();
+            var deletedTuple = tarantoolBoxClient.space("KV").delete(List.of(key)).join();
+            boolean isDeleted = deletedTuple.get() != null && !deletedTuple.get().isEmpty();
+            DeleteResponse response = DeleteResponse.newBuilder().setSuccess(isDeleted).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void range(RangeRequest request, StreamObserver<KeyValuePair> responseObserver) {
+        try {
+            String key_since = request.getKeySince();
+            String key_to = request.getKeyTo();
+            String luaScript = "local key_since, key_to = ... \n" + "local result = {} \n" + "for _, tuple in box.space.KV:pairs({key_since}, {iterator = 'GE'}) do \n" + "    if tuple[1] > key_to then \n" + "        break \n" + "    end \n" + "    table.insert(result, tuple) \n" + "end \n" + "return result";
+            var tarantoolResponse = tarantoolBoxClient.eval(luaScript, Arrays.asList(key_since, key_to)).join();
+            List<?> tuples = (List<?>) tarantoolResponse.get().getFirst();
+            for (Object obj : tuples) {
+                List<?> tuple = (List<?>) obj;
+                String key = (String) tuple.getFirst();
+                Object value = tuple.get(1);
+                KeyValuePair.Builder pair = KeyValuePair.newBuilder().setKey(key);
+                if (value != null) pair.setValue(ByteString.copyFrom((byte[]) value));
+                responseObserver.onNext(pair.build());
+            }
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void count(Empty request, StreamObserver<CountResponse> responseObserver) {
+        try {
+            var tarantoolResponse = tarantoolBoxClient.eval("return box.space.KV:len()").join();
+            long count = ((Number) tarantoolResponse.get().getFirst()).longValue();
+            CountResponse response = CountResponse.newBuilder().setCount(count).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+}
