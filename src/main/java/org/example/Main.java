cfe99a46ffd19c7b3ebd628a347caf6897a014a8
tema <sezemov2@gmail.com>
2026-04-05 01:21:48 +0300

    refactor: add space name and slf4j lombok annotation for logging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,12 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.tarantool.client.box.TarantoolBoxClient;
 import io.tarantool.client.factory.TarantoolFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.example.kv.KvServiceImpl;
 
+@Slf4j
 public class Main {
+    private final static String SPACE_NAME = "KV";
     public static void main(String[] args) throws Exception {
         try (TarantoolBoxClient tarantoolBoxClient = TarantoolFactory.box()
                 .withHost("127.0.0.1")
@@ -27,12 +30,12 @@ public class Main {
             tarantoolBoxClient.eval(initScript).join();
             Server server = ServerBuilder
                     .forPort(9090)
-                    .addService(new KvServiceImpl(tarantoolBoxClient))
+                    .addService(new KvServiceImpl(tarantoolBoxClient, SPACE_NAME))
                     .build();
             server.start();
-            System.out.println("Сервер запущен на порту 9090");
+            log.info("Сервер запущен на порту 9090");
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                System.out.println("Остановка сервера...");
+                log.info("Остановка сервера...");
                 server.shutdown();
             }));
             server.awaitTermination();
